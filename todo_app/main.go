34eb4000bc3ec7fbc1ab2889616d3585c234e3ce
takuro <takur819@gmail.com>
2021-02-20 22:29:04 +0900

    first commit

---
+++ todo_app/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"go_todo/todo_app/app/models"
+)
+
+func main() {
+	fmt.Println(models.Db)
+
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "testtest"
+	fmt.Println(u)
+	u.CreateUser()
+}
