f1d5a94d658d92fb7dd2453824ab2fdcaee24d9d
takuro <takur819@gmail.com>
2021-03-06 13:31:30 +0900

    prepare for #129

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"go_todos/app/controllers"
+	"fmt"
+	"go_todos/app/models"
 )
 
 func main() {
-	controllers.StartMainServer()
+	// controllers.StartMainServer()
+
+	user, _ := models.GetUserByEmail("test@gmail.com")
+	fmt.Println(user)
+
+	// session, err := user.CreateSession()
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+	// fmt.Println(session)
 }
