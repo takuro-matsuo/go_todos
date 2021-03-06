9910d04527546098d7636ddcf9e825251e21444e
takuro <takur819@gmail.com>
2021-03-06 13:27:03 +0900

    prepare for #127

--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"go_todos/app/controllers"
-	"go_todos/app/models"
 )
 
-func TestConnection() {
-
-}
-
 func main() {
-	fmt.Println(models.Db)
-	go controllers.StartMainServer()
-
-	for {
-
-	}
+	controllers.StartMainServer()
 }
